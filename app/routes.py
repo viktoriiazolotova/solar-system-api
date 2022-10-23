ea06d4f91f8d64226188c04815cf2cc83cb2f302
Ariam Yonas <ariam.yonas@hotmail.de>
2022-10-23 11:13:10 -0700

    added specific value error messages

--- app/routes.py
+++ app/routes.py
@@ -24,3 +24,25 @@ def get_all_planets():
                         "color": planet.color}
         result.append(planet_dict)
     return jsonify(result), 200
+
+#WAVE TWO 
+
+@planet_bp.route("/<planet_id>", methods = ["GET"])
+
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        return jsonify({"msg":f'Invalid data type: {planet_id}'}), 400
+    choosing_planet = None 
+    for planet in planet_items:
+        if planet.id == planet_id:
+            choosing_planet = planet 
+    if choosing_planet is None: 
+        return jsonify({"msg":f'Can not find planet id {planet_id}'}), 404
+
+    choosing_planet = {"id": choosing_planet.id,
+                        "name": choosing_planet.name,
+                        "description": choosing_planet.decsription,
+                        "color": choosing_planet.color}
+    return jsonify(choosing_planet), 200
